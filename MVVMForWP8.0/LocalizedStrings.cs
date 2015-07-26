8f9338f61bc782d4f89f3ea92eada9447f9ad946
WP <2381553155@qq.com>
2015-07-26 15:27:02 +0800

    2015-7-26 15:26 添加项目

---
+++ MVVMForWP8.0/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using MVVMForWP8._0.Resources;
+
+namespace MVVMForWP8._0
+{
+    /// <summary>
+    /// 提供对字符串资源的访问权。
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
